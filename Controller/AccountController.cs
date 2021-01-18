813dfa31967622d44137fcafaf26274cc4d8f934
Ashley Walton <7148133+DarkRising101@users.noreply.github.com>
2021-01-18 00:19:02 +0000

    Add project files.

---
+++ Controller/AccountController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace AssignmentWebApp.Controllers
+{
+    [Authorize]
+    [Route("[controller]")]
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        [Route("[controller]/login")]
+        public async Task<IActionResult> LoginAsync()
+        {
+            AuthenticationProperties authProperties = new AuthenticationProperties { RedirectUri = Url.Action("LoginResponse") };
+            await Task.Delay(10);
+            return Challenge(authProperties);
+        }
+
+        
+        public async Task<IActionResult> LoginResponseAsync()
+        {
+            AuthenticateResult result = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            var claims = result.Principal.Identities.FirstOrDefault().Claims;
+            return Json(claims);
+        }
+
+        [Route("[controller]/logout")]
+        public async Task<IActionResult> LogoutAsync()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ Pages/Shared/Components/Navbar/NavbarViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentWebApp.Pages.Shared.Components.Navbar
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return View("LoggedIn");
+            }
+            else
+            {
+                return View("LoggedOut");
+            }
+        }
+    }
+}
